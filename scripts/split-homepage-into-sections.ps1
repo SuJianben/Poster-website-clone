618a6c072845c8c73a13de708c3671dea6df4a72
WJiahao <1814820547@qq.com>
2026-07-30 10:37:42 +0800

    feat: restore product tab interactions

--- scripts/split-homepage-into-sections.ps1
+++ scripts/split-homepage-into-sections.ps1
@@ -113,6 +113,7 @@ $htmlOpen
 {{ content_for_header }}
 $headRenders
 {{ 'source-static-layout.css' | asset_url | stylesheet_tag }}
+<script src="{{ 'source-product-tabs.js' | asset_url }}" defer="defer"></script>
 </head>
 <body>
 <div class="site-wrapper">
